2dc21f37f8a532a4ca1e9c908cbe222c4ba9480c
emeraudesantu <emeraudesantu05@mail.com>
2025-11-19 14:54:54 -0600

    page_main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'page_liste_produits.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: "Atelier Produits",
+      home: ListProduits(),
     );
   }
 }
